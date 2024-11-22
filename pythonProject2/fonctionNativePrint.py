016c40eef75d134bdc9cd9783dec5af99485751a
BAEZA <baeza.julien.lgm@gmail.com>
2024-11-22 13:33:08 +0100

    tp term

---
+++ pythonProject2/fonctionNativePrint.py
@@ -0,0 +1,35 @@
+# coding: UTF-8
+"""
+Script: PythonProject2/fonctionNativePrint
+Création: jbaeza, le 22/11/2024
+"""
+
+
+# Imports
+
+# Fonctions
+
+# Programme principal
+def main():
+
+   """age = 15
+    print("Arthur a", age, "ans.")
+    prenom = 'Arthur'
+    age = 20
+    ville = 'Meylan'
+    print(f'{prenom} a {age} ans et il habite à {ville}.')
+
+    a = 1
+    b = 3
+    c = 8.9
+    mot = 'toto'
+    print(a, b, sep=":", end="/")
+    print(a, b, c, mot, sep=" ;")"""
+
+   b = 3
+   mot = 'toto'
+   print(mot + b * 'titi')
+
+if __name__ == '__main__':
+    main()
+# Fin
