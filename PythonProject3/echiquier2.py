6f1618639d49ac2df44bf9579413f556707d22e6
BAEZA <baeza.julien.lgm@gmail.com>
2024-11-22 14:58:47 +0100

    echiquier2 term

--- PythonProject3/echiquier2.py
+++ PythonProject3/echiquier2.py
@@ -1,16 +1,32 @@
 # coding: UTF-8
 """
-Script: PythonProject3/echiquier2
+Script: Initiation_Python_JBA/echiquier
 Création: jbaeza, le 22/11/2024
 """
-# Imports
 
 # Fonctions
+def echiquier2(colonne, ligne):
+    # Définir les lettres des colonnes
+    colonnes = 'abcdefgh'
+
+    # Boucle pour les lignes de 8 à 1
+    for l in range(8, 0, -1):
+        # Boucle pour les colonnes de a à h
+        for c in colonnes:
+            # Vérifier si la case correspond aux paramètres envoyés
+            if c == colonne and l == ligne:
+                print('*', end=' ')
+            else:
+                print('-', end=' ')
+
+        print()
 
 # Programme principal
 def main():
-    pass
+    # Tester la fonction echiquier2 avec les valeurs colonne = 'd' et ligne = 3
+    print("Affichage de l'échiquier avec une * sur la case d3 :")
+    echiquier2('d', 3)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
 # Fin
