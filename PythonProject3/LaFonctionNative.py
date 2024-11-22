2524504afc009089e80c78d8e25ea35919caa5a6
BAEZA <baeza.julien.lgm@gmail.com>
2024-11-22 14:26:27 +0100

    fonction nativer term

---
+++ PythonProject3/LaFonctionNative.py
@@ -0,0 +1,31 @@
+# coding: UTF-8
+"""
+Script: PythonProject3/LaFonctionNative
+Création: jbaeza, le 22/11/2024
+"""
+
+
+# Imports
+
+# Fonctions
+
+# Programme principal
+def main():
+    #for i in range(5):
+    #for i in range(10, 15):
+    #for i in range(0, 10, 2):
+    for i in range(10,0,-1):
+
+
+        print(i, end='')
+
+    print('\n')
+
+    #for i in [0, 1, 2, 3, 4]:
+    for i in [10,11,12,13,14]:
+    #for i in [0,2,4,6,8]:
+        print(i, end='')
+
+if __name__ == '__main__':
+    main()
+# Fin
