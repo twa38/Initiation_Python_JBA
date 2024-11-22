19b8b6fe49d1a208460c2082caad6b8a690282a1
BAEZA <baeza.julien.lgm@gmail.com>
2024-11-22 13:59:17 +0100

    bouclefor term

---
+++ PythonProject3/boucleFor.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+"""
+Script: PythonProject3/boucleFor
+Création: jbaeza, le 22/11/2024
+"""
+
+
+# Imports
+
+# Fonctions
+
+# Programme principal
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+# Fin
