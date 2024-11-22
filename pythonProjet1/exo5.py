b6c1ec0b3aeb32a5fe6ff39cbe3fc7956f287fa6
BAEZA <baeza.julien.lgm@gmail.com>
2024-11-22 13:42:25 +0100

    tp1

---
+++ pythonProjet1/exo5.py
@@ -0,0 +1,31 @@
+# coding: UTF-8
+"""
+Script: Initiation_Python_JBA/exo5
+Création: jbaeza, le 22/11/2024
+"""
+
+# Imports
+
+# Fonctions
+
+# Programme principal
+def main():
+    # Initialiser la température extérieure
+    temperature = 10
+
+
+    soleil = 0
+
+
+    if temperature < 5 or temperature > 25:
+        print("volets fermes")
+    elif temperature < 15 and soleil == 0:
+        print("volets fermes")
+    elif soleil == 1 and temperature > 20:
+        print("volets demi fermes")
+    else:
+        print("volets ouverts")
+
+if __name__ == '__main__':
+    main()
+# Fin
---
+++ pythonProjet1/exo6.py
@@ -0,0 +1,27 @@
+# coding: UTF-8
+"""
+Script: Initiation_Python_JBA/exo6
+Création: jbaeza, le 22/11/2024
+"""
+
+# Imports
+
+# Fonctions
+
+# Programme principal
+def main():
+
+    caractere = 'j'
+
+
+    nb_lignes = 10
+
+
+
+
+    for i in range(1, nb_lignes ):
+        print(caractere * i)
+
+if __name__ == '__main__':
+    main()
+# Fin
