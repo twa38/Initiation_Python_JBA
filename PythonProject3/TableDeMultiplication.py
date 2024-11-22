f40ff44c89bc529fa139da004e3f9c9c11fe2df2
BAEZA <baeza.julien.lgm@gmail.com>
2024-11-22 15:55:00 +0100

    Table term

--- PythonProject3/TableDeMultiplication.py
+++ PythonProject3/TableDeMultiplication.py
@@ -1,16 +1,25 @@
 # coding: UTF-8
 """
-Script: PythonProject3/TableDeMultiplication
+Script: Initiation_Python_JBA/multiplication
 Création: jbaeza, le 22/11/2024
 """
-# Imports
 
 # Fonctions
+def table():
+
+    for i in range(1, 11):
+
+        for j in range(1, 11):
+
+            print(i * j, end='\t')
+
+        print()
 
 # Programme principal
 def main():
-    pass
+    # Appeler la fonction table pour afficher la table de multiplication
+    table()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
 # Fin
